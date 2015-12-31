edf5d6383f87684a51534cc8d5701ae5c0def5ab
seethersan <carlos_jcez@hotmail.com>
2015-12-31 15:21:09 -0500

    Agregando archivos de aplicacion userprofile

---
+++ forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.http import HttpResponseRedirect
+
+class UserCreationEmailForm(UserCreationForm):
+	email = forms.EmailField(required=True)
+
+	class Meta:
+		model = User
+		fields = ('username', 'email')
+
+	def __init__(self, *args, **kwargs):
+		self.user_cache = None
+		super(UserCreationEmailForm, self).__init__(*args,**kwargs)
+
+	def clean(self):
+		email = self.cleaned_data.get('email')
+		try:
+			self.user_cache = User.objects.get(email=email)
+			raise forms.ValidationError('Email ya registrado')
+			return None
+		except User.DoesNotExist:
+			return self.cleaned_data
+
+	def get_user(self):
+		return self.user_cache
+
+class EmailAuthenticationForm(forms.Form):
+	email = forms.EmailField(required=True)
+	password = forms.CharField(required=True, widget=forms.PasswordInput)
+
+	def __init__(self, *args, **kwargs):
+		self.user_cache = None
+		super(EmailAuthenticationForm, self).__init__(*args,**kwargs)
+
+	def clean(self):
+		email = self.cleaned_data.get('email')
+		password = self.cleaned_data.get('password')
+
+		self.user_cache = authenticate(email=email, password=password)
+
+		if self.user_cache is None:
+			raise forms.ValidationError('Usuario Incorrecto')
+		elif not self.user_cache.is_active:
+			raise forms.ValidationError('Usuario Inactivo')
+		return self.cleaned_data
+
+	def get_user(self):
+		return self.user_cache
---
+++ views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .forms import UserCreationEmailForm, EmailAuthenticationForm
+from django.contrib.auth import login
+
+# Create your views here.
+def signup(request):
+	form = UserCreationEmailForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+	return render(request, 'signup.html', {'form': form})
+
+def signin(request):
+	form = EmailAuthenticationForm(request.POST or None)
+	if form.is_valid():
+		login(request, form.get_user())
+		#logear
+	return render(request, 'signin.html', {'form':form})
